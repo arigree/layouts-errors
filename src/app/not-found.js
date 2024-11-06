8a4d5b9936b1138c71eb83e7121cbf9fa4b613e3
arigree <arigree@iu.edu>
2024-11-06 09:32:31 -0500

    project setup, page not found page created

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function PageNoteFound(){
+    return(
+        <main>
+            <h1>Page Not Found</h1>
+            <p>Try using the right URL</p>
+        </main>
+    )
+}
