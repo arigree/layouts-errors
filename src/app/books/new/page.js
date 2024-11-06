702e137a652674129581a9ecffef44864689fa3f
arigree <arigree@iu.edu>
2024-11-06 10:24:17 -0500

    form

--- src/app/books/new/page.js
+++ src/app/books/new/page.js
@@ -1,20 +1,33 @@
 "use client";
 
 export default function NewBookPage() {
+  function onAddBook(e) {
+    e.preventDefault();
+
+    const formData= new FormData(e.target);
+
+    console.log("form data", formData.get("bookName"))
+  }
+
   return (
     <main>
       <h1>New Book</h1>
-      <form>
+      <form onSubmit={onAddBook}>
         <div>
           <label htmlFor="bookName">Name</label>
           <input type="text" id="bookName" name="bookName"></input>
         </div>
         <div>
           <label htmlFor="bookCover">Cover Image</label>
-          <input type="file" accept="image/*" id="bookCover" name="bookCover"></input>
+          <input
+            type="file"
+            accept="image/*"
+            id="bookCover"
+            name="bookCover"
+          ></input>
         </div>
         <div>
-            <button type="submit" value="Add Book"/>
+          <button type="submit" value="Add Book">Add Book</button>
         </div>
       </form>
     </main>
--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,9 +1,11 @@
 "use client";
+import Link from "next/link";
 
 export default function BooksPage() {
   return (
     <main>
       <h1>Book</h1>
+      <Link href="/books/new">Add New Book</Link>
     </main>
   );
 }
