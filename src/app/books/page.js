4630a23650db5991fb2200084a69a17e234cad5a
arigree <arigree@iu.edu>
2024-11-06 10:10:09 -0500

    removed error and not foung pages from books

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,28 +1,9 @@
 "use client";
 
-import Error from "next/error";
-import BooksError from "./error";
-
-export default function BooksPage(){
-    
-        try{
-            function throwError(){
-            console.log("Bad Books");
-        throw new Error("Bad Books"); 
-        }
-        return(
-        <main>
-            <h1>
-                Book
-            </h1>
-
-            <button onClick={throwError}>Open Bad Books</button>
-        </main>
-    );
-        
-    }catch(e){
-        return <BooksError error={e}/>
-    }
-
-   
+export default function BooksPage() {
+  return (
+    <main>
+      <h1>Book</h1>
+    </main>
+  );
 }
